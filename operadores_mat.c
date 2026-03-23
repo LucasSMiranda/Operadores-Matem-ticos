202d5fa45b86cffce6994a31d013b9ec8d4f33f7
Lucas Miranda <104878497+LucasSMiranda@users.noreply.github.com>
2026-03-23 23:15:00 +0000

    Adicionado linhas de prog. para fazer soma

--- operadores_mat.c
+++ operadores_mat.c
@@ -2,7 +2,18 @@
 
 int main () {
 
+    int numero1, numero2;
+    int soma;
 
+    printf ("Digite número 1:\n");
+    scanf("%d", &numero1);
+
+    printf("Digite o número 2;\n");
+    scanf("%d", &numero2);
+
+    soma=numero1+numero2;
+
+    printf("O resultado é %d\n", soma);
 
 
     return 0;
